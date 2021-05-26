3158ce627319fcbb981555a9d2c2607f58e676a1
Tech-Mex6 <61053657+Tech-Mex6@users.noreply.github.com>
2021-05-26 14:01:12 -0500

    Project 18 - Debugging

---
+++ Project18/Project18/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Project18
+//
+//  Created by meekam okeke on 11/23/20.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        for i in 1...100{
+            print("i is now \(i).")
+        }
+    }
+}
+
